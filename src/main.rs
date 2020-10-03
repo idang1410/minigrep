f080600ba4bbf205b461718948b515afd0c06c49
Idan <idang1410@gmail.com>
2020-10-04 01:59:18 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::process;
+use std::env;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config  = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem with arguments : {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Error : {}", e);
+
+        process::exit(2);
+    }
+}
+
+
